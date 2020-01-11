a1565ea4d482a51a101ab0b010a92d59a494cb01
Alison Vermeil <alison.minton@gmail.com>
2020-01-11 13:49:20 -0700

    Add assertions in validations

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe Coupon do
   describe "validations" do
     it {should validate_presence_of :name}
+    # this could have a limited number of characters
     it {should validate_presence_of :code}
     it {should validate_presence_of :percent}
+    #do you need to validate both presence of and numericality of?
+    it {should validate_numericality_of :percent, greater_than_or_equal_to: 0}
+    it {should validate_numericality_of :percent, less_than_or_equal_to: 1}
+    it {should validate_uniqueness_of :name}
+    it {should validate_uniqueness_of :code}
   end
 
   describe "relationships" do
