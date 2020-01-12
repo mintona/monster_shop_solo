2c8c6b484bcf87ace1058b74862d657039d8bdeb
Alison Vermeil <alison.minton@gmail.com>
2020-01-12 12:11:37 -0700

    Add coupon create route and action

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -11,6 +11,19 @@ class Merchant::CouponsController < Merchant::BaseController
     @coupon = Coupon.new
   end
 
+  def create
+    merchant = Merchant.find(current_user.merchant_id)
+    @coupon = merchant.coupons.new(coupon_params)
+
+    if @coupon.save
+      flash[:success] = "Coupon has been added!"
+      redirect_to merchant_coupons_path
+    else
+      flash[:error] = "#{@coupon.errors.full_messages.to_sentence}. Please try again."
+      render :new
+    end
+  end
+
   private
     def coupon_params
       params.require(:coupon).permit(:name, :code, :percent)
