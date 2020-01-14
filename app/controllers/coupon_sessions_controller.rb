e6cc95c0167c4774adf8f73a9e9c73e6412ad456
Alison Vermeil <alison.minton@gmail.com>
2020-01-13 19:32:44 -0700

    Change flash message to indicate coupon has been applied to cart instead of order

--- app/controllers/coupon_sessions_controller.rb
+++ app/controllers/coupon_sessions_controller.rb
@@ -9,7 +9,7 @@ class CouponSessionsController < ApplicationController
       items = Item.find(item_ids)
       if items.any? { |item| item.merchant_id == coupon.merchant_id }
         session[:coupon_id] = coupon.id
-        flash[:success] = "#{coupon.code} has been applied to your order."
+        flash[:success] = "#{coupon.code} has been applied to your cart."
       else
         flash[:error] = "Coupon cannot be applied to your items. Please add items from #{coupon.merchant.name} and re-enter code or try another code."
       end
