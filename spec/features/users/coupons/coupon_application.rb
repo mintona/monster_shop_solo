1811e2a1c25a20efe2200d217a43824e85c14b44
Alison Vermeil <alison.minton@gmail.com>
2020-01-13 19:12:22 -0700

    Add sad path and edge case tests for coupon appplication spec

--- spec/features/users/coupons/coupon_application.rb
+++ spec/features/users/coupons/coupon_application.rb
@@ -6,16 +6,16 @@ RSpec.describe "As a in user" do
     before :each do
       @user = create(:user)
 
-      store_1 = create(:merchant)
+      @store_1 = create(:merchant)
       # @merchant = create(:user, role: 1, merchant: store)
 
-      @coupon_1 = create(:coupon, percent: 20, merchant: store_1)
+      @coupon_1 = create(:coupon, percent: 20, merchant: @store_1)
 
       # @order = create(:order, user: @user)
 
-      @item_1 = create(:item, inventory: 20, price: 9, merchant: store_1) #11
-      @item_2 = create(:item, inventory: 25, price: 9, merchant: store_1) #12
-      @item_3 = create(:item, inventory: 30, price: 9, merchant: store_1) #13
+      @item_1 = create(:item, inventory: 20, price: 9, merchant: @store_1) #11
+      @item_2 = create(:item, inventory: 25, price: 9, merchant: @store_1) #12
+      @item_3 = create(:item, inventory: 30, price: 9, merchant: @store_1) #13
 
 
       items = [@item_1, @item_2, @item_3]
@@ -28,10 +28,12 @@ RSpec.describe "As a in user" do
       # @order.item_orders.create(item: @item_2, quantity: 10, price: @item_2.price)
       # @order.item_orders.create(item: @item_3, quantity: 10, price: @item_3.price)
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 #Add test for when there are no items in the cart
     it "I see a box where I can enter my coupon code" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
       visit cart_path
 
       within "#apply-coupon" do
@@ -41,6 +43,8 @@ RSpec.describe "As a in user" do
     end
 
     it "if I enter a valid coupon code and click submit I am alerted that coupon is now applied to my order" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
       visit cart_path
 
       expect(page).to have_content("Total: $27.00")
@@ -65,6 +69,8 @@ RSpec.describe "As a in user" do
       @item_6 = create(:item, inventory: 30, price: 10, merchant: store_2) #16
 
       store_2_items = [@item_4, @item_5, @item_6]
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
       store_2_items.each do |item|
         visit "/items/#{item.id}"
@@ -92,6 +98,8 @@ RSpec.describe "As a in user" do
       store_2 = create(:merchant)
       store_2_coupon = create(:coupon, percent: 15, merchant: store_2)
 
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
       visit cart_path
 
       within "#apply-coupon" do
@@ -104,6 +112,8 @@ RSpec.describe "As a in user" do
     end
 
     it "I am alerted if I enter an invalid coupon code" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
       visit cart_path
 
       within "#apply-coupon" do
@@ -115,10 +125,122 @@ RSpec.describe "As a in user" do
       expect(page).to have_content("The coupon code you have entered does not exist. Please try again.")
     end
 
-    describe "and I have already entered and submitted a coupon code" do
-      xit "I can enter a new coupon which will replace the one I entered previously" do
+#TEST FOR LOGGING OUT AND NOT COMING BACK TO A COUPON ALREADY LOGGED IN.
 
+    describe "and I have already entered and submitted a coupon code" do
+      it "I can enter a new coupon which will replace the one I entered previously" do
+        coupon_2 = create(:coupon, percent: 30, merchant: @store_1)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+        visit cart_path
+
+        expect(page).to have_content("Total: $27.00")
+
+        within "#apply-coupon" do
+          fill_in "Code", with: @coupon_1.code
+          click_button 'Submit'
+        end
+
+        expect(current_path).to eq(cart_path)
+        expect(page).to have_content("#{@coupon_1.code} has been applied to your order.")
+
+        expect(page).to have_content("Total: $27.00")
+        expect(page).to have_content("Discounted Total: $21.60")
+        expect(page).to have_content("Coupon Applied: #{@coupon_1.code}")
+
+        within "#apply-coupon" do
+          fill_in "Code", with: coupon_2.code
+          click_button 'Submit'
+        end
+
+        expect(page).to have_content("Total: $27.00")
+        expect(page).to have_content("Discounted Total: $18.90")
+        expect(page).to have_content("Coupon Applied: #{coupon_2.code}")
+
+        expect(page).to_not have_content("Discounted Total: $21.60")
+        expect(page).to_not have_content("Coupon Applied: #{@coupon_1.code}")
       end
+    end
+
+    it "the coupon code I entered is remembered and applied to new items if I leave the cart page, continue shopping, and re-visit the cart" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+      visit cart_path
+
+      expect(page).to have_content("Total: $27.00")
+
+      within "#apply-coupon" do
+        fill_in "Code", with: @coupon_1.code
+        click_button 'Submit'
+      end
+
+      expect(page).to have_content("Total: $27.00")
+      expect(page).to have_content("Discounted Total: $21.60")
+      expect(page).to have_content("Coupon Applied: #{@coupon_1.code}")
+
+      click_on "All Items"
+      click_on "#{@item_1.name}"
+      click_on "Add To Cart"
+      click_on "Cart: 4"
+
+      expect(page).to have_content("Total: $36.00")
+      expect(page).to have_content("Discounted Total: $28.80")
+      expect(page).to have_content("Coupon Applied: #{@coupon_1.code}")
+    end
+
+    it "my coupon code I entered is not remembered when I log out and log back in" do
+      visit cart_path
+
+      expect(page).to have_content("Total: $27.00")
+
+      click_link "Login"
+
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: "password"
+      click_button 'Submit'
+
+      visit cart_path
+
+      within "#apply-coupon" do
+        fill_in "Code", with: @coupon_1.code
+        click_button 'Submit'
+      end
+
+      expect(page).to have_content("#{@coupon_1.code} has been applied to your order.")
+
+      click_link 'Log Out'
+
+      click_link "Login"
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: "password"
+      click_button 'Submit'
+
+      visit cart_path
+
+      expect(page).to have_content("Cart is currently empty")
+
+      items = [@item_1, @item_2, @item_3]
+
+      items.each do |item|
+        visit "/items/#{item.id}"
+        click_on "Add To Cart"
+      end
+
+      visit cart_path
+      expect(page).to_not have_content("#{@coupon_1.code} has been applied to your order.")
+    end
+  end
+
+  describe "when I have no items in my cart" do
+    it "I cannot enter a coupon code" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit cart_path
+
+      expect(page).to_not have_field("Code")
+      expect(page).to_not have_button("Submit")
     end
   end
 end
