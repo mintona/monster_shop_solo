103cea67ebc7a0cb6440ad239e634eebb790b1ec
Alison Vermeil <alison.minton@gmail.com>
2020-01-14 09:49:15 -0700

    Add code for discounted_total, to be refactored

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order <ApplicationRecord
 
   belongs_to :user
   belongs_to :coupon, optional: true
-  
+
   has_many :item_orders
   has_many :items, through: :item_orders
 
@@ -11,6 +11,25 @@ class Order <ApplicationRecord
 
   def grandtotal
     item_orders.sum('price * quantity')
+  end
+
+  def discounted_total
+    # item_orders = self.item_orders
+    # items_matching_coupon_merchant = items.where(merchant_id: coupon.merchant_id)
+    ids = items.where(merchant_id: coupon.merchant_id).pluck(:id)
+    multiplier = (100 - coupon.percent)/100.to_f
+    # item_orders_with_discounted_items = item_orders.where(item_id: ids)
+    # regular_total = item_orders_with_discounted_items.sum('price * quantity')
+    regular_total = item_orders.where(item_id: ids).sum('price * quantity')
+    discounted_total = regular_total * multiplier
+
+    dollars_off = regular_total - discounted_total
+    # discounted_total = item_orders.where(item_id: ids).sum('price * quantity') * multiplier
+    # grandtotal - discounted_total
+    grandtotal - dollars_off
+    # require "pry"; binding.pry
+
+# require "pry"; binding.pry
   end
 
   def total_items
