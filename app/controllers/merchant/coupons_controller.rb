5b12f4fd74d3fe273f5a4c2a04defb811abd9147
Alison Vermeil <alison.minton@gmail.com>
2020-01-14 21:21:24 -0700

    Refactor update method into private methods

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -28,14 +28,12 @@ class Merchant::CouponsController < Merchant::BaseController
   end
 
   def update
-    @coupon = Coupon.find(params[:format])
-
-    if @coupon.update(coupon_params)
-      flash[:success] = "Coupon has been updated!"
-      redirect_to merchant_coupons_path
+    if info_update?
+      @coupon = Coupon.find(params[:format])
+      attempt_info_update(@coupon)
     else
-      flash[:error] = "#{@coupon.errors.full_messages.to_sentence}. Please try again."
-      render :edit
+      @coupon = Coupon.find(params[:id])
+      update_status(@coupon)
     end
   end
 
@@ -59,5 +57,28 @@ class Merchant::CouponsController < Merchant::BaseController
         flash[:error] = "#{@coupon.errors.full_messages.to_sentence}. Please try again."
         render :new
       end
+    end
+
+    def info_update?
+      params[:status].nil?
+    end
+
+    def attempt_info_update(coupon)
+      if coupon.update(coupon_params)
+        flash[:success] = "Coupon has been updated!"
+        redirect_to merchant_coupons_path
+      else
+        flash[:error] = "#{coupon.errors.full_messages.to_sentence}. Please try again."
+        render :edit
+      end
+    end
+
+    def update_status(coupon)
+      if params[:status] = "deactivate"
+        coupon.toggle!(:active?)
+      else
+        coupon.toggle!(:active?)
+      end
+      redirect_to merchant_coupons_path
     end
 end
